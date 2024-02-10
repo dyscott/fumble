1ee93bc4ce8b4f48a40cfc563def8939824bc4af
Gretta Halollari <grettahalollari@gmail.com>
2024-02-10 00:32:31 -0500

    scared for my code rn

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,121 +1,137 @@
 import 'package:flutter/material.dart';
-//import 'package:pocketbase/pocketbase.dart';
-import 'package:provider/provider.dart';
+// import 'bioform.dart';
+import 'confirm.dart';
 
-import 'auth.dart';
-import 'pages/create/page1.dart';
-
-void main() async {
-  // Load shared preferences and initialize PocketBase
-
-  runApp(ChangeNotifierProvider(
-    create: (context) => AuthProvider(),
-    child: const MainApp(),
+void main() {
+  runApp(MaterialApp(
+    home: ConfirmPage(),
   ));
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    final loading = Provider.of<AuthProvider>(context).loading;
+// import 'package:flutter/material.dart';
+// //import 'package:pocketbase/pocketbase.dart';
+// import 'package:provider/provider.dart';
 
-    return MaterialApp(
-        home: loading
-            ? const Scaffold(body: Center(child: CircularProgressIndicator()))
-            : const Home());
-  }
-}
+// import 'auth.dart';
+// import 'pages/create/page1.dart';
+// import 'bioform.dart';
 
-class Home extends StatelessWidget {
-  const Home({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    final isAuth = Provider.of<AuthProvider>(context).isAuthenticated;
 
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('Fumble'),
-            const SizedBox(height: 20),
-            Container(
-              margin: const EdgeInsets.only(top: 20),
-              child: isAuth ? const Go() : const SignIn(),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
 
-class SignIn extends StatelessWidget {
-  const SignIn({super.key});
+// void main() async {
+//   // Load shared preferences and initialize PocketBase
 
-  @override
-  Widget build(BuildContext context) {
-    final auth = Provider.of<AuthProvider>(context);
-    return ElevatedButton(
-        onPressed: () {
-          // Sign in
-          auth.signIn();
-        },
-        child: const Text('Sign In / Register'));
-  }
-}
+//   runApp(ChangeNotifierProvider(
+//     create: (context) => AuthProvider(),
+//     child: const MainApp(),
+//   ));
+// }
 
-class Go extends StatelessWidget {
-  const Go({super.key});
+// class MainApp extends StatelessWidget {
+//   const MainApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    final auth = Provider.of<AuthProvider>(context);
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => CreateProfilePage1()),
-            );
-          },
-          child: const Text('Go'),
-        ),
-        ElevatedButton(
-          onPressed: () {
-            auth.signOut();
-          },
-          child: const Text('Sign Out'),
-        ),
-      ],
-    );
-  }
-}
-
-// class DisplayUser extends StatelessWidget {
-//   final String userId;
-//   const DisplayUser({required this.userId, super.key});
-
-//   Future<RecordModel> getUser() {
-//     return pb.collection('users').getOne(userId);
-//   }
-  
 //   @override
 //   Widget build(BuildContext context) {
-//     return FutureBuilder<RecordModel>(
-//       future: getUser(),
-//       builder: (context, snapshot) {
-//         if (snapshot.connectionState == ConnectionState.done) {
-//           return Text('User: ${snapshot.data?.data['username']}');
-//         } else {
-//           return const CircularProgressIndicator();
-//         }
-//       },
+//     final loading = Provider.of<AuthProvider>(context).loading;
+
+//     return MaterialApp(
+
+//         home: loading
+//             ? const Scaffold(body: Center(child: CircularProgressIndicator()))
+//             : const Home());
+//   }
+// }
+
+// class Home extends StatelessWidget {
+//   const Home({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     final isAuth = Provider.of<AuthProvider>(context).isAuthenticated;
+
+//     return Scaffold(
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             const Text('Fumble'),
+//             const SizedBox(height: 20),
+//             Container(
+//               margin: const EdgeInsets.only(top: 20),
+//               child: isAuth ? const Go() : const SignIn(),
+//             ),
+//           ],
+//         ),
+//       ),
 //     );
 //   }
 // }
+
+// class SignIn extends StatelessWidget {
+//   const SignIn({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     final auth = Provider.of<AuthProvider>(context);
+//     return ElevatedButton(
+//         onPressed: () {
+//           // Sign in
+//           auth.signIn();
+//         },
+//         child: const Text('Sign In / Register'));
+//   }
+// }
+
+// class Go extends StatelessWidget {
+//   const Go({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     final auth = Provider.of<AuthProvider>(context);
+//     return Column(
+//       mainAxisAlignment: MainAxisAlignment.center,
+//       children: [
+//         ElevatedButton(
+//           onPressed: () {
+//             Navigator.push(
+//               context,
+//               MaterialPageRoute(builder: (context) => CreateProfilePage1()),
+//             );
+//           },
+//           child: const Text('Go'),
+//         ),
+//         ElevatedButton(
+//           onPressed: () {
+//             auth.signOut();
+//           },
+//           child: const Text('Sign Out'),
+//         ),
+//       ],
+//     );
+//   }
+// }
+
+// // class DisplayUser extends StatelessWidget {
+// //   final String userId;
+// //   const DisplayUser({required this.userId, super.key});
+
+// //   Future<RecordModel> getUser() {
+// //     return pb.collection('users').getOne(userId);
+// //   }
+  
+// //   @override
+// //   Widget build(BuildContext context) {
+// //     return FutureBuilder<RecordModel>(
+// //       future: getUser(),
+// //       builder: (context, snapshot) {
+// //         if (snapshot.connectionState == ConnectionState.done) {
+// //           return Text('User: ${snapshot.data?.data['username']}');
+// //         } else {
+// //           return const CircularProgressIndicator();
+// //         }
+// //       },
+// //     );
+// //   }
+// // }
