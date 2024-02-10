9490114a6ce1d84ba1f87195c0ab30edafaa5393
Dylan Scott <dylanscott821@gmail.com>
2024-02-10 00:23:11 -0500

    File Upload

---
+++ client/lib/pages/create/create_profile.dart
@@ -0,0 +1,5 @@
+class Profile {
+  final String? name;
+
+  Profile({this.name});
+}
--- client/lib/pages/create/page1.dart
+++ client/lib/pages/create/page1.dart
@@ -1,25 +1,47 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class CreateProfilePage1 extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class CreateProfilePage1 extends StatefulWidget {
+  const CreateProfilePage1({super.key});
+
+  @override
+  State<CreateProfilePage1> createState() => _CreateProfilePage1State();
+}
+
+class _CreateProfilePage1State extends State<CreateProfilePage1> {
+  File? image;
+
+  Future pickImage() async {
+    var imagePicker = ImagePicker();
+    var pickedImage = await imagePicker.pickImage(
+      source: ImageSource.gallery,
+    );
+    if (pickedImage != null) {
+      setState(() {
+        image = File(pickedImage.path);
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Create Profile'),
-      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('Create Profile Page 1'),
-            ElevatedButton(
-              child: Text('Next'),
-              onPressed: () {
-                // Navigator.push(
-                //   context,
-                //   MaterialPageRoute(builder: (context) => CreateProfilePage2()),
-                // );
+          children: [
+            GestureDetector(
+              onTap: () {
+                pickImage();
               },
+              child: CircleAvatar(
+                  backgroundColor: Colors.grey,
+                  radius: 80,
+                  child: image != null
+                      ? Image.file(image!)
+                      : const Icon(Icons.add_a_photo, size: 50, color: Colors.white)),
             ),
           ],
         ),
