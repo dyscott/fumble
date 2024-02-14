1b993d731111168356992c21f41e5244e06977bb
Dylan Scott <dylanscott821@gmail.com>
2024-02-13 22:26:11 -0500

    Update rsync

--- server/rsync.sh
+++ server/rsync.sh
@@ -1,4 +1,4 @@
 rm -rf pb_public/web
 cp -r ../client/build/web pb_public
-rsync -avz -e ssh . opc@129.158.61.254:/home/opc/pb
-#rsync -avz -e ssh pb_public/web opc@129.158.61.254:/home/opc/pb/pb_public
+#rsync -avz -e ssh . opc@129.158.61.254:/home/opc/pb
+rsync -avz -e ssh pb_public/web opc@129.158.61.254:/home/opc/pb/pb_public
